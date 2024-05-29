71b00d90d2ca34e4f4420d3064a355ae2f09001b
Your Name <you@example.com>
2024-05-29 21:47:09 +0530

    shifting char

--- EncryptDecrypt.java
+++ EncryptDecrypt.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 class EncryptDecrypt{
     public static void main(String [] args){
-        System.out.println("Hello world");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        int key = 3;
+        for(int i = 0; i < input.length(); i++){
+            char output = (char)((int)input.charAt(i) + key);
+            System.out.print(output);
+        }
     }
 }
