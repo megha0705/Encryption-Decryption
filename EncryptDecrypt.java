816adc29e890f45f0564b974ce6b9f4a65ce2396
Megha <chakrabortyreshmi077@gmail.com>
2024-05-30 20:30:29 +0530

    decryption function added

--- EncryptDecrypt.java
+++ EncryptDecrypt.java
@@ -1,20 +1,42 @@
 import java.util.Scanner;
 class EncryptDecrypt{
     public static StringBuilder encrypted(String input , int key){
-         StringBuilder encoded = new StringBuilder();
-         for(int i = 0; i < input.length(); i++){
+        StringBuilder encoded = new StringBuilder();
+        for(int i = 0; i < input.length(); i++){
             char encodedChar = (char)(((int)input.charAt(i) - 65 + key) % 26 + 65 );
             encoded.append(encodedChar);
         }
-         encoded.toString();
-         System.out.print(encoded);
-         return encoded;
+        encoded.toString();
+        System.out.print(encoded);
+        return encoded;
+    }
+
+    public static StringBuilder decrypted(String input, int key){
+        StringBuilder decoded = new StringBuilder();
+        for (int i = 0; i < input.length(); i++){
+            char decodedChar = (char)(((int)input.charAt(i) + 65 - key)% 26 + 65);
+            decoded.append(decodedChar);
+        }
+        decoded.toString();
+        System.out.println(decoded);
+        return decoded;
     }
 
     public static void main(String [] args){
-        Scanner sc = new Scanner(System.in);
-        String input = sc.next();
         int key = 3;
-        encrypted(input , key);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("do u want to decrypt or encrypt string?");
+        String choice = sc.nextLine();
+        if(choice.equalsIgnoreCase("encrypt")){
+            String input = sc.next();
+            encrypted(input , key);
+        }
+        else if(choice.equalsIgnoreCase("decrypt")){
+            String input = sc.next();
+            decrypted(input , key);
+        }
+        else{
+            System.out.println("invalid");
+         }
     }
 }
