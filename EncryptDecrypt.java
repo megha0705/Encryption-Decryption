e56c5ef1972be10c51be5b6c663cd482d74fbd68
Megha <chakrabortyreshmi077@gmail.com>
2024-05-30 12:39:11 +0530

    encryption function

--- EncryptDecrypt.java
+++ EncryptDecrypt.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 class EncryptDecrypt{
+    public static StringBuilder encrypted(String input , int key){
+         StringBuilder encoded = new StringBuilder();
+         for(int i = 0; i < input.length(); i++){
+            char encodedChar = (char)(((int)input.charAt(i) - 65 + key) % 26 + 65 );
+            encoded.append(encodedChar);
+        }
+         encoded.toString();
+         System.out.print(encoded);
+         return encoded;
+    }
+
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
         String input = sc.next();
         int key = 3;
-      /*  for(int i = 0; i < input.length(); i++){
-            char output = (char)(((int)input.charAt(i) - 65 + key) % 26 + 65 );
-            System.out.print(output);
-        }*/
-        for (int i = 0; i < input.length(); i++){
-            char output = (char)(((int)input.charAt(i) + 65 - key)% 26 + 65);
-            System.out.print(output);
-        }
+        encrypted(input , key);
     }
 }
