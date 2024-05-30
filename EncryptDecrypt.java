2ff24431491334815848d1a3a108511b45061b54
Your Name <you@example.com>
2024-05-30 10:55:38 +0530

    decryption

--- EncryptDecrypt.java
+++ EncryptDecrypt.java
@@ -4,8 +4,12 @@ class EncryptDecrypt{
         Scanner sc = new Scanner(System.in);
         String input = sc.next();
         int key = 3;
-        for(int i = 0; i < input.length(); i++){
+      /*  for(int i = 0; i < input.length(); i++){
             char output = (char)(((int)input.charAt(i) - 65 + key) % 26 + 65 );
+            System.out.print(output);
+        }*/
+        for (int i = 0; i < input.length(); i++){
+            char output = (char)(((int)input.charAt(i) + 65 - key)% 26 + 65);
             System.out.print(output);
         }
     }
