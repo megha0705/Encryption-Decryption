7f335be6a773e0aaa4000c9962aed9360664802c
Megha <chakrabortyreshmi077@gmail.com>
2024-05-31 20:24:58 +0530

    numbers encrypted and decrypted

--- EncryptDecrypt.java
+++ EncryptDecrypt.java
@@ -3,19 +3,35 @@ class EncryptDecrypt{
     public static StringBuilder encrypted(String input , int key){
         StringBuilder encoded = new StringBuilder();
         for(int i = 0; i < input.length(); i++){
-            char encodedChar = (char)(((int)input.charAt(i) - 65 + key) % 26 + 65 );
-            encoded.append(encodedChar);
+            char encodedChar = input.charAt(i);
+            if(Character.isDigit(encodedChar)){
+                int asciiValue = (int)encodedChar;
+                char encodedNum = (char)((asciiValue - 48 + key) % 10 + 48);
+                encoded.append(encodedNum);
+             }else{
+                char encodedString = (char)(((int)encodedChar - 65 + key)% 26 + 65);
+                encoded.append(encodedString);
+             }
         }
         encoded.toString();
         System.out.print(encoded);
         return encoded;
-    }
+}
+ 
 
     public static StringBuilder decrypted(String input, int key){
         StringBuilder decoded = new StringBuilder();
         for (int i = 0; i < input.length(); i++){
-            char decodedChar = (char)(((int)input.charAt(i) + 65 - key)% 26 + 65);
-            decoded.append(decodedChar);
+            char decodedChar = (char)input.charAt(i);
+            if(Character.isDigit(decodedChar)){
+                int asciiValue = (int)decodedChar;
+                char decodedNum = (char)((asciiValue + 52 - key) % 10 + 48);
+                decoded.append(decodedNum);
+            }else{
+                char decodedString = (char)(((int)input.charAt(i) + 65 - key)% 26 + 65);
+                decoded.append(decodedString);
+
+            }
         }
         decoded.toString();
         System.out.println(decoded);
